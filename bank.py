969aed62b29022e03cd4e12a8c5a2bf2dd6485d1
Dorothy2020 <wangarraakoth@gmail.com>
2021-06-07 22:40:14 +0300

    pythondict

--- bank.py
+++ bank.py
@@ -12,17 +12,17 @@ class BankAccount:
         self.name=name
         self.balance=0
         self.statement=[]
-        
+        self.loan=0
        
     def save(self):
         return f"I will add 300 in my {self.fixed}"
     def withdraw(self):
         return f"I will withdraw 100 in my {self.balance}"
     def show_balance(self):
-        return f"Hello,{self.name} your balance is {self.balance}"
+        return f"Your {self.name} balance is {self.balance}"
     def deposit(self,amount):
         if amount<=0:
-           return "you can nnot deposit"
+           return "you can cannot deposit"
         else:
 
           
@@ -62,6 +62,47 @@ class BankAccount:
     
     def repay(self,amount):
         return f"Hello {self.name} I have repaid {amount} "
+
+    def borrow(self,amount):
+            # self.amount=amount
+            # self.loan=0
+            if amount<0:
+                return f"You can borrow  {self.amount} if you qualify for a loan{amount}"
+            elif self.loan>0:
+                return f"You don't have a loan {self.loan}"
+            elif amount<0.1 *self.balance:
+                return f"You cannot borrow ksh {amount}"
+            
+            else:
+                loan=amount*1.05
+                self.loan=loan
+                self.balance+=amount
+                return f"You have successfuly borrowed {self.loan}"
+
+    def repay(self,amount):
+        # self.amount=amount
+        if amount<0:
+            return f"You have an extra amount {amount} in the bank"
+        
+        elif amount<self.loan:
+            self.loan-=amount
+            now=datetime.now()
+            borrow_transaction={"amount":amount,"time":now,"Narration" :"you have borrowed ksh"}
+            self.statement.append(borrow_transaction)
+            return f" Hello {self.loan} decrease loan with {amount}"
+
+        else :
+            diff=amount-self.loan
+            self.loan=0
+            self.deposit(diff) 
+            now=datetime.now()
+            repay_transaction={"amount":amount,"time":now,"Narration" :"repaid loan sucessfully ksh"}
+            self.statement.append(repay_transaction)
+            return f"You have fully repaid your loan"       
+
+
+
+
     
 
     
